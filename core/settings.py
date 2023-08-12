fc755fea8042f56b1b6f152242a23cad22625f78
Maamoun.Mribah <mribahmaamoun@gmail.com>
2023-08-12 17:41:16 +0100

    Update settings.py
    hope

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', [::1], django-images.azurewebsites.net]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'django-images.azurewebsites.net']
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(' ')
 
 SECURE_SSL_REDIRECT = \
